03bd5ffb2013568931ca249419ad39645753f529
Frida Rojo <fridarojo@Fridas-MacBook-Air-2.local>
2024-04-06 15:29:36 -0500

    lab part 2

---
+++ sqllab/createtable.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS earthquakes;
+CREATE TABLE earthquakes (
+  quakedate date,
+  quaketime time with time zone,
+  latitude real,
+  longitude real,
+  quakedepth real,
+  mag real,
+  magError real,
+  locationSource text,
+  mageSource text,
+  place text,
+);
