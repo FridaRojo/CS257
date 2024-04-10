8b448a3f5906ae0fd2635bc650d8e072a1431d66
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-10 14:15:36 -0500

    Update createtable.sql

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
+  quakedate text,
   latitude real,
   longitude real,
   quakedepth real,
