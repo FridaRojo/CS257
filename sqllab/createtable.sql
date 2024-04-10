661757bdb93d06c2c5d7085a24879a2181c17749
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-09 22:10:47 -0500

    lol actually fixed syntax

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,11 +1,12 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
   quakedate date,
+  quaketime time with time zone,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
-  magError text,
+  magError real,
   locationSource text,
   mageSource text,
   place text
