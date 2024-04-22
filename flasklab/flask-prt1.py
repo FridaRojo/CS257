f879088d8ef5ec9cf9c6eec5fb0cb654bb285a4c
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 10:58:23 -0500

    added str

--- flasklab/flask-prt1.py
+++ flasklab/flask-prt1.py
@@ -6,6 +6,7 @@ app = flask.Flask(__name__)
 @app.route('/add/<num1>/<num2>')
 def my_adding(num1,num2):
   added_num = int(num1) + int(num2)
+  added_num = str(added_num)
   return num1 + "+" + num2 + "=" + added_num
 
 if __name__ == '__main__':
