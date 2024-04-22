fa8e1c5a2c6f824daef024853221d8bca1646d4e
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 10:50:04 -0500

    adding issue

--- flasklab/flask-prt1.py
+++ flasklab/flask-prt1.py
@@ -5,8 +5,8 @@ app = flask.Flask(__name__)
 #
 @app.route('/add/<num1>/<num2>')
 def my_adding(num1,num2):
-  added_num = num1 + num2
-  return num1 + "+" + num2 + "=" + added_num
+  added_num = num1+num2
+  return num1,"+",num2,"=",added_num
 
 if __name__ == '__main__':
     my_port = 5225
