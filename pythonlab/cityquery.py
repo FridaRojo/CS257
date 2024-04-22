9734f0b48de92bb5b90e28d40ef0adcd4c98dd94
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 13:45:02 -0500

    Update cityquery.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,3 +1,35 @@
-def querys:
-  
+import psycopg2
+
+def querys():
+  conn = psycopg2.connect(
+    host="localhost",
+    port=5432,   
+    database="rojof",
+    user="rojof",
+    password="spoon387ardi")
+
+    if conn is not None:
+        print( "Connection Worked!" )
+    else:
+        print( "Problem with Connection" )
+
+    return None 
+  )
+
+  cursor = conn.cursor()
+
+    # Determining if Northfield is present in the database
+    cursor.execute("SELECT city, state, lat, long FROM cities WHERE city = 'Northfield';")
+    northfield_result = cursor.fetchone()
+
+    if northfield_result:
+        print("Northfield is present in the database. Location (Latitude, Longitude):", northfield_result[2], northfield_result[3])
+    else:
+        print("Northfield is not present in the database.")
+      
+
+
+
+
+
 
