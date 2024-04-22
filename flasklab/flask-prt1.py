b3a1179c5e115dbd0f83a0a18a1f609e468c9844
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 10:45:16 -0500

    added > to flask-prt1.py

--- flasklab/flask-prt1.py
+++ flasklab/flask-prt1.py
@@ -2,7 +2,8 @@ import flask
 
 app = flask.Flask(__name__)
 
-@app.route('/add/num1/num2')
+#
+@app.route('/add/num1>/num2>')
 def my_adding(num1,num2)
   added_num = num1 + num2
   return num1 + "+" + num2 + "=" + added_num
