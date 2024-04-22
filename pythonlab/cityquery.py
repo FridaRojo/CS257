b069e2d40a6353d2e809ac0cb41f32b57b46cb0d
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 17:33:04 -0500

    mn city with smallest pop added

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -24,10 +24,17 @@ def querys():
   else:
     print("Northfield is not present in the database.")
 
+    print()
+
    #Printing out the name of the city with the largest population
     cursor.execute("SELECT city FROM cities ORDER BY pop DESC LIMIT 1;")
     largest_pop_city = cursor.fetchone()[0]
     print("City with largest population:", largest_pop_city)
+
+    #Printing out name of Minnesota city with the smallest pop
+    cursor.execute("SELECT city FROM cities WHERE state = 'Minnesota' BY pop DESC LIMIT 1;")
+    smallest_pop_city_mn = cursor.fetchone()[0]
+    print("Minnesota city with the smallest population:", smallest_pop_city_mn)
 
     
 
