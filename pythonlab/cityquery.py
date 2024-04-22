f60df30a6b12efb0d9a0e77013541bcbd1167f7a
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 16:53:03 -0500

    indent syntax

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -8,24 +8,23 @@ def querys():
     user="rojof",
     password="spoon387ardi")
 
-    if conn is not None:
-        print( "Connection Worked!" )
-    else:
-        print( "Problem with Connection" )
+  if conn is not None:
+    print( "Connection Worked!" )
+  else:
+    print( "Problem with Connection" )
 
-    return None 
+  return None 
   )
 
   cursor = conn.cursor()
 
-    # Determining if Northfield is present in the database
-    cursor.execute("SELECT city, state, lat, long FROM cities WHERE city = 'Northfield';")
-    northfield_result = cursor.fetchone()
-
-    if northfield_result:
-        print("Northfield is present in the database. Location (Latitude, Longitude):", northfield_result[2], northfield_result[3])
-    else:
-        print("Northfield is not present in the database.")
+  # Determining if Northfield is present in the database
+  cursor.execute("SELECT city, state, lat, long FROM cities WHERE city = 'Northfield';")
+  northfield_result = cursor.fetchone()
+  if northfield_result:
+    print("Northfield is present in the database. Location (Latitude, Longitude):", northfield_result[2], northfield_result[3])
+  else:
+    print("Northfield is not present in the database.")
       
 
 
