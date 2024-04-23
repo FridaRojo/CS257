b038a75fc0d77333c5d3cbd2993f446909d36428
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-22 20:10:42 -0500

    added [0]

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -59,9 +59,9 @@ def queries():
     state = input("Enter a state/state abbreviation: ")
     state_query = "SELECT SUM(c.pop) AS total_pop FROM cities c JOIN states s ON c.state = s.code OR c.state = s.state WHERE lower(s.state) = lower(%s)"
     cursor.execute(state_query, (state,))
-    total_pop = cursor.fetchone()
+    total_pop = cursor.fetchone()[0]
     if total_pop:
-      print(state, "'s total population: ", total_pop)
+      print(state,"'s total population: ", total_pop)
     else:
       print(state, " is not in the database.")
 
