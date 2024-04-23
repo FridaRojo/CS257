9d18da60b70769d7a6df7b062136974aa385e0da
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-04-23 13:41:20 -0500

    clarity and comments

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+#All queries funciton
 def queries():
   conn = psycopg2.connect(
     host="localhost",
@@ -8,15 +9,15 @@ def queries():
     user="rojof",
     password="spoon387ardi")
 
+  #Testing connection
   if conn is not None:
     print( "Connection Worked!" )
   else:
     print( "Problem with Connection" )
   print()
-  
   cursor = conn.cursor()
 
-  # Determining if Northfield is present in the database
+  #Determining if Northfield is present in the database
   cursor.execute("SELECT city, state, lat, long FROM cities WHERE city = 'Northfield';")
   northfield_result = cursor.fetchone()
   if northfield_result:
@@ -29,6 +30,7 @@ def queries():
     cursor.execute("SELECT city FROM cities ORDER BY pop DESC LIMIT 1;")
     largest_pop_city = cursor.fetchone()[0]
     print("City with largest population:", largest_pop_city)
+    print()
 
     #Printing out name of Minnesota city with the smallest pop
     cursor.execute("SELECT city FROM cities WHERE state = 'Minnesota' ORDER BY pop LIMIT 1;")
@@ -59,12 +61,12 @@ def queries():
     state_input = input("Enter a state/state abbreviation: ")
     cursor.execute("SELECT state FROM states WHERE code ILIKE %s OR state ILIKE %s;", (state_input, state_input))
     state_row = cursor.fetchone()
-
+    #Making it so that either inputs work
     if state_row:
       full_name = state_row[0]
       cursor.execute("SELECT SUM(c.pop) AS total_population FROM cities c JOIN states s ON c.state = s.code OR c.state = s.state WHERE s.state ILIKE %s;", (full_name,)) 
       total_pop = cursor.fetchone()
-   
+      #Prints the total population of input or if there is no information
       if total_pop[0]:
         print(state_input,"'s total population: ", total_pop[0])
       else:
