aac1595f66deb1d8a94ac66f5faeb07305003692
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-05-08 10:19:12 -0500

    Create example.py

---
+++ csslab/example.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+  message =  "hello and welcome"
+
+
+if __name__ == '__main__':
+    my_port = 5225
+    app.run(host='0.0.0.0', port = my_port) 
