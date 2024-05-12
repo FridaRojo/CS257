22f5b4ab1cbd6770e7f7833362501b0ae00717a4
FridaRojo <165401736+FridaRojo@users.noreply.github.com>
2024-05-11 21:45:40 -0500

    added match()

--- csslab/example.py
+++ csslab/example.py
@@ -8,6 +8,10 @@ def welcome():
   message =  "Match is a fun game where we try and get all the same numbers in the same color!"
   return render_template("home.html", text = message)
 
+@app.route('/newpage')
+def match():
+  return renter_template("match.html")
+
 if __name__ == '__main__':
     my_port = 5225
     app.run(host='0.0.0.0', port = my_port) 
